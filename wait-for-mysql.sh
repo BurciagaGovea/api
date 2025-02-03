44d973e8e2d76319d5b57e0b0361dbe8422121fc
BurciagaGovea <rodrigovea0@gmail.com>
2025-02-02 22:43:26 -0600

    Verificar variables

--- wait-for-mysql.sh
+++ wait-for-mysql.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 set -e
 
-echo "⏳ Esperando a que MySQL esté disponible..."
+# Validar que las variables necesarias están definidas
+if [ -z "$MYSQLDB_HOST" ]; then
+    echo "❌ Error: La variable MYSQLDB_HOST no está definida."
+    exit 1
+fi
+
+echo "⏳ Esperando a que MySQL esté disponible en $MYSQLDB_HOST:3306..."
 while ! nc -z "$MYSQLDB_HOST" 3306; do
     echo "⌛ MySQL aún no está listo. Esperando..."
     sleep 2
